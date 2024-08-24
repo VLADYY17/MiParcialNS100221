0a3f9edbfba5aaae26371211fca4665365b0ee58
KeVelo <kevkevinsango@gmail.com>
2024-08-24 16:11:21 -0600

    Commit

---
+++ MiParcialitoB/MiParcialitoB/Models/Cursos.cs
@@ -0,0 +1,11 @@
+﻿namespace MiParcialitoB.Models
+{
+    public class Cursos
+    {
+        public int CursoID { get; set; }  // Clave primaria
+        public string NombreCurso { get; set; }
+
+        // Relación uno a muchos con Inscripciones
+        public ICollection<Inscripciones> Inscripcion { get; set; }
+    }
+}
---
+++ MiParcialitoC/MiParcialitoC/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Login(string email, string password)
+    {
+        return RedirectToAction("Menu");
+    }
+
+    public IActionResult Menu()
+    {
+        return View();
+    }
+}
